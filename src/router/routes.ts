78bc6fc258f0df9e1f5de8e2b15ad16adbf38cec
Javier A Mora gil <j4viermora@gmail.com>
2024-10-03 02:17:08 -0300

    feat: config profile route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,6 +1,13 @@
 import { RouteRecordRaw } from "vue-router";
 import HomeView from "../views/home.vue";
+import ProfileView from "../views/profile.vue";
 
-const routes: RouteRecordRaw[] = [{ path: "", component: HomeView }];
+const routes: RouteRecordRaw[] = [
+    { path: "", component: HomeView },
+    {
+        path: "/profile",
+        component: ProfileView,
+    },
+];
 
 export { routes };
